574083e41b2ef8788d5887052c85659ea942424e
karthikeyan_P@contus <karthikeyan.p@contus.in>
2020-02-25 16:17:43 +0530

    LoginVIew Controller is added

---
+++ SecondTestProject/SecondTestProject/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  SecondTestProject
+//
+//  Created by user on 25/02/20.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Login View Controller is Added")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
